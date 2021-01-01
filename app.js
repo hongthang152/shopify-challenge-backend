b9a0e4b6b265ae3516d68270fe0614411df94623
Thang Nguyen <nguyenhongthang1998@gmail.com>
2020-12-31 21:50:51 -0500

    Revert CORS

--- app.js
+++ app.js
@@ -15,13 +15,7 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-app.use((req, res, next) => {
-      res.setHeader('Access-Control-Allow-Origin', '*');
-      res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-      res.setHeader('Access-Control-Allow-Headers', '"Origin, X-Requested-With, Content-Type, Accept"');
-      res.setHeader('Access-Control-Allow-Credentials', true);
-      next();
-});
+app.use(cors({ origin: '*' }));
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
